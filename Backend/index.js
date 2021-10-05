ede5045bc1924288f3194f222b345bb80b900293
Iuri N. Matiello <82899922+IuriDev071@users.noreply.github.com>
2021-10-05 01:39:19 -0300

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,49 +1,39 @@
 const express = require('express')
 const mongoose = require('mongoose')
-// const axios = require('axios').create({baseURL: "https://cep.awesomeapi.com.br/%22%7Dr"})
-const User = mongoose.model('User',  { email: String, nome: String, senha: String})
-mongoose.connect('mongodb+srv://Iuri:<password>@cluster0.ndtwj.mongodb.net/test')
-
+// const axios = require('axios').create({baseURL: "https://localhost:3000"})
 const app = express()
+const port = 3002
+const User = mongoose.model('User', {nome: String, senha: String})
 
 app.use(express.json())
 
-app.post('/cadastro', (req, res) => {
-    
-    const {email, senha} = res.json(req.body)
+mongoose.connect('mongodb+srv://Admin:<password>@cluster0.ndtwj.mongodb.net/test')
 
-    if (email != null && senha != null) {
+app.use(express.static(__dirname + '/private/frontend'))
 
+app.get('/', (req, res) => {
+    res.json('Backend funcionado')
+      res.status(200, 'Ok')
+})
+
+app.get('/login', (req, res) => {
+    res.json(req.body)
+     res.status(200, 'OK')
+})
+
+app.get('/usuarios', (req, res) => {
+    if(nome != null || senha != null) {
         const newUser = new User(req.body)
-
-        newUser.save()
-        .then(() => {
-            console.log("Good")
-        }).catch(() => {
-            console.log("Bad")
-        })
-
-        res.status(201, 'Created').json(req.body)
-
-    } else {
-
-        return res.status(400, 'Bad request').json( {mensagem: "O campo de email e senha estão incorretos"} )
-
+          newUser.save()
+          .then((result) => {
+              console.log(result)
+          }).catch((error) => {
+              console.log(error)
+          })
+            res.status(201, 'Created')
     }
 })
 
-app.get('/usuarios/login', async (req, res) => {
-    const usuarios = await User.find()
-
-    res.status(200, 'OK')
-    res.json(usuarios)
-})
-
-// app.delete('/usuarios', (req, res) => {
-//     res.status(200, 'DELETE')
-//     res.json(req.body)
-// })
-
-app.listen(3001, () => {
-    console.log("Rodando na porta 3001.\nDigite localhost:3001")
+app.listen(port, () => {
+    console.log("Rodando na porta 3002\nDigite a URL: localhost:3000")
 })
