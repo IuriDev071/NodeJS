e721d916460b87e1d8987cd129f1d95e0d50eb79
Iuri N. Matiello <82899922+IuriDev071@users.noreply.github.com>
2021-10-19 19:19:55 -0300

    Add files via upload

--- Login/app.js
+++ Login/app.js
@@ -5,9 +5,9 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3005
 
-const User = mongoose.model('User', {email: String, senha: String})
+const User = mongoose.model('User', {email: String, senha: String, nome: String})
 
-mongoose.connect('mongodb+srv://Iuri:iurinm890@cluster0.ndtwj.mongodb.net/test')
+mongoose.connect('mongodb+srv://admin:iurinm890@cluster0.ofjky.mongodb.net/test')
 
 app.use(express.json())
 
@@ -16,32 +16,33 @@ app.get('/', (req, res) => {
 })
 
 app.post('/usuario', (req, res) => {
-    const {email, senha} = req.query
+    const {nome, email, senha} = req.query
     const newUser = new User(req.query)
-    if (email != null && senha != null) {
-        newUser.save(req.query)
-        .then((result) => {
-            res.json(result)
-        }).catch((error) => {
-            console.log(error)
-        })
-        return res.status(201, 'Created').json(req.query)
-    } else {
-        return res.status(400, 'Bad request').json(error)
-    }
+
+      if (email != null && senha != null && nome != null) {
+            newUser.save(req.query)
+            .then((result) => {
+                res.json(result)
+            }).catch((error) => {
+                console.log(error)
+            })
+            return res.status(201, 'Created').json(req.query)
+        } else {
+            return res.status(400, 'Bad request')
+        }
 })
 
 app.get('/usuario/login/', async (req, res) => {
     const {email, senha} = req.query
-    const usuarios = await User.find()
+    const usuarios = await User.find(email, senha)
     res.status(200, 'Ok').json(usuarios)
 })
 
-app.delete('/usuario/remove/', async (req, res) => {
-    const {email, senha} = req.query
-    const usersDel = await User.remove()
-    res.status(202, 'Accepted').json(usersDel)
-})
+// app.delete('/usuario/remove/', async (req, res) => {
+//     const {email, senha, nome} = req.query
+//     const usersDel = await User.remove(email, senha, nome)
+//     res.status(202, 'Accepted').json(usersDel)
+// })
 
 app.listen(port, () => {
     console.log('Rodando na porta 3005')
